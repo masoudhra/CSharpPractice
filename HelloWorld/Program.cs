2b1cdf3133767cb92711dbc92707325f27941f30
Masoud Ramezani <masoudhra@gmail.com>
2023-07-10 11:32:43 +1200

    practice C#

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Please insert your age ");
+             string input =  Console.ReadLine();
+             int age = Convert.ToInt32(input);
+
+             if(age < 18)
+             {
+                 Console.WriteLine("you are not allowed to continue!");
+             }
+             else
+             {
+                 Console.WriteLine("Wellcome to capadocia!");
+             }
+             Console.ReadKey();*/
+
+            Console.WriteLine("Please enter your number");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if(number %2 == 0)
+            {
+                Console.WriteLine("Your number is even");
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd");
+            }
+            Console.ReadKey();
+          
+
+
+            
+
+        }
+    }
+}
